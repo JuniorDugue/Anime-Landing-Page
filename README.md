7f4bfe0504477c1636eb50906261ba5dbfaf6322
juniordugue <jrdugue@gmail.com>
2020-08-09 16:09:47 -0400

    Installed and imported swr, included api, rendered trending anime titles on page

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Anime-Landing-Page
+react app landing page using the swr hook and api from kitsu
+
+api from https://kitsu.docs.apiary.io/#reference/anime/anime
+
+swr hook from https://swr.vercel.app/
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import useSWR from "swr";
 import "./App.css";
 
+const API_URL = "https://kitsu.io/api/edge/trending/anime";
+
 function App() {
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(API_URL, fetcher);
+  // console.log(data);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  console.log(data);
+
   return (
     <div className="App">
       <h1>Anime Landing Page</h1>
+      <div>
+        {data.data.map(anime => {
+          return (
+            <h2>{anime.attributes.canonicalTitle}</h2>
+          )
+        })}
+      </div>
     </div>
   );
 }
