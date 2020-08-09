795c779ad0a8e4762dfc1d6f51032dd81ff50906
juniordugue <jrdugue@gmail.com>
2020-08-09 18:17:22 -0400

    added additional animations to the other properties on the page

--- client/src/components/AnimeCard.js
+++ client/src/components/AnimeCard.js
@@ -14,10 +14,26 @@ function AnimeCard({ data }) {
 
         return (
           <div key={anime.id}>
-            <img className="card-poster" src={medium} alt="poster" />
-            <motion.h2 className="card-title">{canonicalTitle}</motion.h2>
-            <motion.p className="card-description">{synopsis.substring(0, 150)}....</motion.p>
-            <motion.p className="card-rating">{averageRating}</motion.p>
+            <motion.img 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{ duration: 0.9 }}
+            className="card-poster" src={medium} alt="poster" />
+            <motion.h2 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{ duration: 0.9 }}
+            className="card-title">{canonicalTitle}</motion.h2>
+            <motion.p 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{ duration: 0.9 }}
+            className="card-description">{synopsis.substring(0, 150)}....</motion.p>
+            <motion.p 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{ duration: 0.9 }}
+            className="card-rating">{averageRating}</motion.p>
           </div>
         );
       })}
