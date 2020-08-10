3974fdf8182d152641361e2eedad828e4861e80d
juniordugue <jrdugue@gmail.com>
2020-08-10 01:02:54 -0400

    reformatted the components using prettier

--- client/src/components/AnimeCard.js
+++ client/src/components/AnimeCard.js
@@ -11,29 +11,18 @@ function AnimeCard({ data }) {
           synopsis,
           posterImage: { medium },
         } = anime.attributes;
-
         return (
           <div key={anime.id}>
-            <motion.img 
-            initial={{opacity: 0, x: '-100vw'}}
-            animate={{opacity: 1, x: 0}}
-            transition={{ duration: 0.9, delay: 1.0, type: 'spring', stiffness: 50 }}
-            className="card-poster" src={medium} alt="poster" />
-            <motion.h2 
-            initial={{opacity: 0, x: '-100vw'}}
-            animate={{opacity: 1, x: 0}}
-            transition={{ duration: 0.9, delay: 1.2, type: 'spring', stiffness: 50 }}
-            className="card-title">{canonicalTitle}</motion.h2>
-            <motion.p 
-            initial={{opacity: 0, x: '-100vw'}}
-            animate={{opacity: 1, x: 0}}
-            transition={{ duration: 0.9, delay: 1.6, type: 'spring', stiffness: 50 }}
-            className="card-description">{synopsis.substring(0, 150)}....</motion.p>
-            <motion.p 
-            initial={{opacity: 0, x: '-100vw'}}
-            animate={{opacity: 1, x: 0}}
-            transition={{ duration: 0.9, delay: 1.8, type: 'spring', stiffness: 50 }}
-            className="card-rating">{averageRating}</motion.p>
+            <motion.img initial={{ opacity: 0, x: "-100vw" }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.9, delay: 1.0, type: "spring", stiffness: 50 }} className="card-poster" src={medium} alt="poster" />
+            <motion.h2 initial={{ opacity: 0, x: "-100vw" }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.9, delay: 1.2, type: "spring", stiffness: 50 }} className="card-title">
+              {canonicalTitle}
+            </motion.h2>
+            <motion.p initial={{ opacity: 0, x: "-100vw" }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.9, delay: 1.6, type: "spring", stiffness: 50 }} className="card-description">
+              {synopsis.substring(0, 150)}....
+            </motion.p>
+            <motion.p initial={{ opacity: 0, x: "-100vw" }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.9, delay: 1, type: "spring", stiffness: 50 }} className="card-rating">
+              {averageRating}
+            </motion.p>
           </div>
         );
       })}
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,15 +8,10 @@ function Header({ data }) {
 
   return (
     <div className="header">
-      <motion.h1 
-      animate={{fontSize: 40, color: 'orange', }}
-      transition={{delay: .1, type: 'spring', stiffness: 320}}
-      >
-      Jr's Anime Landing Page</motion.h1>
-      <motion.img 
-      animate={{ scale: [0, 1] }}
-      transition={{ duration: 0.5 }}
-      className="header-img" src={small} alt="banner" />
+      <motion.h1 animate={{ fontSize: 40, color: "orange" }} transition={{ delay: 0.1, type: "spring", stiffness: 320 }}>
+        Jr's Anime Landing Page
+      </motion.h1>
+      <motion.img animate={{ scale: [0, 1] }} transition={{ duration: 0.5 }} className="header-img" src={small} alt="banner" />
     </div>
   );
 }
